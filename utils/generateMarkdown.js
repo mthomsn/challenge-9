5dad46b44a5b65842dd79345845d28aec535d715
Matt <matthewthomsen99@gmail.com>
2022-10-19 20:26:33 -0500

    add code to license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return license ? `https://img.shields.io/badge/License-${license}-f39f37` 
+  : ``;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return license ? `- [${license}](${license})` : ``;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return license ? `## License
+  [${license}](https://choosealicense.com/licenses/${license})`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -16,22 +24,29 @@ function generateMarkdown(data) {
   # ${data.title}
   // put license badge here
 
-  # Description
+  ## Description
   ${data.description}
 
-  # Installation
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#Contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
+
+  ## Installation
   ${data.install}
 
-  # Usage
+  ## Usage
   ${data.usage}
 
-  # Contribution
+  ## Contribution
   ${data.contribution}
 
-  # Testing
+  ## Testing
   ${data.testInstruction}
 
-  # Questions
+  ## Questions
   ${data.username}
   ${data.email}
   `;
